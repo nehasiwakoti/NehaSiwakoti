1ee313f94444a272273b3fd753772c06e2bda6f9
neha shiwakoti <siwakotineha22@gmail.com>
2020-12-07 13:26:43 +0545

    Fronrend

---
+++ Amazona/frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen  from './screens/HomeScreen';
+
+function App() {
+  return (    
+      <BrowserRouter>    
+    <div className="grid-container">
+        <header className="row">
+            <div>
+              <a className="brand" href="/">amazona</a>
+               </div>
+                <div>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">Sign In</a>
+                </div>
+            </header>
+
+        <main>
+      
+            <Route path="/" exact component={HomeScreen}></Route> 
+            <Route path="/product/:id"  component={ProductScreen}></Route>         
+         </main>
+
+        <footer className=" row center">All right reserved  </footer>
+        </div> 
+        </BrowserRouter>
+  );
+}
+export default App;
